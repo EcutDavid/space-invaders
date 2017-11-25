ba5f31daa30909dce9070858d26897533b38b343
David Guan <davidguandev@gmail.com>
2017-11-25 18:16:06 +1100

    Improve particles effects

--- Assets/Scripts/invaderController.cs
+++ Assets/Scripts/invaderController.cs
@@ -4,33 +4,39 @@ using UnityEngine;
 
 public class InvaderController : MonoBehaviour {
 	private bool died = false;
-	private Quaternion angularVelocity;
+	private Vector3 angularVelocity;
 	public AudioClip diedAudio;
 	public int score = 10;
+	public ParticleSystem Particles;
+
+	private float diedSpeedY = 12f;
 
 	void Start () {
-		angularVelocity = new Quaternion(
-			Random.Range(1f, 30f),
-			Random.Range(1f, 30f),
-			Random.Range(1f, 30f),
-			0
+		angularVelocity = new Vector3(
+			Random.Range(10f, 30f),
+			Random.Range(3f, 9f),
+			Random.Range(3f, 9f)
 		);
 	}
 	
 	void Update () {
 		if (died) {
-			this.transform.rotation = new Quaternion (
-				this.transform.rotation.x + angularVelocity.x * Time.deltaTime,
-				this.transform.rotation.y + angularVelocity.y * Time.deltaTime,
-				this.transform.rotation.z + angularVelocity.z * Time.deltaTime,
-				this.transform.rotation.w
-			);
+			var angles = transform.rotation.eulerAngles;
+			angles.x += angularVelocity.x;
+			angles.y += angularVelocity.y;
+			angles.z += angularVelocity.z;
+			transform.rotation = Quaternion.Euler(angles);
+
 			this.transform.position = new Vector3 (
 				this.transform.position.x,
-				this.transform.position.y + 30f * Time.deltaTime,
+				this.transform.position.y + diedSpeedY * Time.deltaTime,
 				this.transform.position.z
 			);
 			// TODO: makes the invader looks smaller or add some particles?
+			var cameraOrthographicSize = Camera.main.orthographicSize;
+			if (System.Math.Abs (transform.position.y) > (cameraOrthographicSize + 0.8)) {
+				Destroy (this.gameObject);
+			}
 		}
 	}
 
@@ -44,6 +50,8 @@ public class InvaderController : MonoBehaviour {
 			var collider = GetComponent<MeshCollider> ();
 			Destroy (collider);
 			tag = "Died";
+			var particles = Instantiate (Particles);
+			particles.transform.position = transform.position;
 			GetComponent<AudioSource> ().PlayOneShot (diedAudio);
 			FindObjectOfType<GameController> ().score += score;
 			if (GameObject.FindGameObjectsWithTag ("Invader").Length == 0) {
